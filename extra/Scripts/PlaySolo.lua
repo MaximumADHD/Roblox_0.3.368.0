daa71b67a28e7f1c6325da491c8f60b2201141d3
Max <clonet1019@gmail.com>
2021-03-16 00:59:23 -0500

    Reorganized, added extra content to help inexperienced users.

---
+++ extra/Scripts/PlaySolo.lua
@@ -0,0 +1,37 @@
+local player = game.Players.LocalPlayer
+
+if not player then
+	player = game.Players:createLocalPlayer(0)
+end
+
+local function waitForChild(parent,childName)
+	local child
+	
+	while true do
+		child = parent:findFirstChild(childName)
+		
+		if child then
+			break
+		else
+			parent.ChildAdded:wait()
+		end
+	end
+	
+	return child
+end
+
+local function onChanged(property)
+	if property == "Character" then
+		local humanoid = waitForChild(player.Character, "Humanoid")
+		
+		humanoid.Died:connect(function ()
+			wait(5)
+			player:LoadCharacter()
+		end)
+	end
+end
+
+player.Changed:connect(onChanged)
+player:LoadCharacter()
+
+game:service("RunService"):run()
